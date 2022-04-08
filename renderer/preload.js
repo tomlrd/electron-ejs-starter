190af361eb50149541db2573c1b59ff6fa16ebfc
tom <thomas.laroudie@gmail.com>
2022-04-08 08:59:38 +0200

    nocdn

--- renderer/preload.js
+++ renderer/preload.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log('Preload.js');
   // Expose des fonctions d'envoi IPC mais PAS l'api electron 
+  // https://www.electronjs.org/fr/docs/latest/tutorial/ipc#2-expose-ipcrendereron-via-preload
+  // todo : ipcMain.invoke system
   const { ipcRenderer, contextBridge } = require('electron');
   contextBridge.exposeInMainWorld("api", {
     send: (channel, data) => ipcRenderer.send(channel, data),
